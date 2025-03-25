a5e5a53d9912bd6a602183271b84a8d7af76ba9b
Alaye-Dong <AlayeVast@outlook.com>
2025-03-25 18:13:50 +0800

    feat: 将数据转存到MongoDB而不是json文件

--- scrapy_dangdang/scrapy_dangdang/pipelines.py
+++ scrapy_dangdang/scrapy_dangdang/pipelines.py
@@ -7,23 +7,32 @@
 # useful for handling different item types with a single interface
 
 
+# 导入MongoDB的Python驱动
+from pymongo import MongoClient
+
 class ScrapyDangdangPipeline:
-    # 当Spider开启时执行初始化方法
+    def __init__(self):
+        # 连接到MongoDB服务器
+        self.client = MongoClient('localhost', 27017)
+        # 选择数据库
+        self.db = self.client['dangdang']
+        # 选择集合
+        self.collection = self.db['books']
+
     def open_spider(self, spider):
-        # 打开或创建一个名为book.json的文件，用于存储爬取的数据
-        self.fp = open("book.json", "w", encoding="utf-8")
+        # 可以在这里进行一些初始化操作
+        pass
 
-    # item: yield的book对象
     def process_item(self, item, spider):
-        # 将爬取到的item（书籍信息）转换为字符串形式并写入文件
-        self.fp.write(str(item))
-
+        # 将爬取到的item（书籍信息）插入到MongoDB集合中
+        self.collection.insert_one(dict(item))
         # 继续传递item给下一个Pipeline处理
         return item
 
     def close_spider(self, spider):
-        # 当Spider关闭时，关闭文件写入流
-        self.fp.close()
+        # 关闭MongoDB连接
+        self.client.close()
+
 
 # 导入urllib库中的request模块，用于发送网络请求
 import urllib.request
